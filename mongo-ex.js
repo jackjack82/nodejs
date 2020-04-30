a5aaf818b2fff63bfb4cce6350c273bd4bdcbf54
jackjack82 <giacomo.grasso.82@gmail.com>
2020-04-30 12:44:30 +0200

    [mongo-ex.js] ObjectID and find() and id management

--- mongo-ex.js
+++ mongo-ex.js
@@ -1,11 +1,19 @@
 // from course, CRUD methods
 
-const mongodb  = require('mongodb')
-const MongoClient = mongodb.MongoClient
-
+// const mongodb  = require('mongodb')
+// const MongoClient = mongodb.MongoClient
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'mongo_course'
 
+// const ObjectId = mongodb.ObjectID
+
+// we create the ID ourselve
+const {MongoClient, ObjectID} = require('mongodb')
+
+// https://docs.mongodb.com/manual/reference/method/ObjectId/ or
+// http://mongodb.github.io/node-mongodb-native/3.6/api/ObjectID.html
+const id = new ObjectID()
+console.log(id)
 
 // http://mongodb.github.io/node-mongodb-native/3.6/api/MongoClient.html
 MongoClient.connect(connectionUrl, { userNewUrlparser: true }, (error,client) => {
@@ -18,8 +26,9 @@ MongoClient.connect(connectionUrl, { userNewUrlparser: true }, (error,client) =>
     // http://mongodb.github.io/node-mongodb-native/3.6/api/Collection.html
     // collection('xx').insertMany([{xxx},{xxx}])
     db.collection('users').insertOne({
-         name: 'Giacomo',
-         age: 27,
+        _id: id , 
+        name: 'Malcom',
+         age: 37,
     }, (error, result) => {
         if(error){
             return console.log('Unable to insert user')
